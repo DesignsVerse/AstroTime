55e4f8f3492a504965fc8dc5e5218c97fcabc7e3
DesignsVerse <designsverseagency@gmail.com>
2025-07-23 01:29:08 +0530

    Update page.tsx

--- app/panchang/page.tsx
+++ app/panchang/page.tsx
@@ -5,8 +5,8 @@ import React from 'react'
 const page = () => {
   return (
     <div className='pt-14'>
-{/*       <PanchangNumerologyPage/> */}
-      <PanchangFetcher/>
+      <PanchangNumerologyPage/>
+{/*       <PanchangFetcher/> */}
     </div>
   )
 }
